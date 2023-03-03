6624f86952f2323f08dd1904a97bb7edcaeadb80
Rutvik Ahire <dev@rutvikahire.com>
2023-03-03 01:22:34 -0500

    updates

--- src/run.py
+++ src/run.py
@@ -2,4 +2,4 @@ from application import create_app
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(host='localhost', port=8888, debug=True)
+    app.run(host='localhost', port=5000, debug=True)
